644cdc75db777f5c85b8ac86b9ed3a5538f5545b
DESKTOP-3M8EM7B\zed <iahsnil1994@gmail.com>
2019-05-20 19:52:35 +0700

    xiaozhuan font generator

---
+++ nine/src/main/resources/pyscript/output_xiaozhuan.py
@@ -0,0 +1,52 @@
+
+from PIL import Image, ImageDraw, ImageFont
+'''
+    Auth: Xiaowu Chen
+    Note: Please install [pillow] library before run this script.
+'''
+ 
+ 
+def draw_image(new_img, text, show_image=False):
+    text = str(text)
+    draw = ImageDraw.Draw(new_img)
+    img_size = new_img.size
+ 
+    font_size = 40
+    fnt = ImageFont.truetype('xiao.ttf', font_size)
+    fnt_size = fnt.getsize(text)
+    while fnt_size[0] > img_size[0] or fnt_size[0] > img_size[0]:
+        font_size -= 5
+        fnt = ImageFont.truetype('xiao.ttf', font_size)
+        fnt_size = fnt.getsize(text)
+ 
+    x = (img_size[0] - fnt_size[0]) / 2
+    y = (img_size[1] - fnt_size[1]) / 2
+    draw.text((x, y), text, font=fnt, fill=(0, 0, 0))
+ 
+    if show_image:
+        new_img.show()
+    del draw
+ 
+ 
+def new_image(width, height, text='default', color=(255, 255, 255, 255), show_image=False):
+    new_img = Image.new('RGBA', (int(width), int(height)), color)
+    draw_image(new_img, text, show_image)
+    new_img.save(r'%s_%s_%s.png' % (width, height, text))
+    del new_img
+ 
+ 
+def new_image_with_file(fn):
+    with open(fn, encoding='utf-8') as f:
+        for l in f:
+            l = l.strip()
+            if l:
+                ls = l.split(',')
+                if '#' == l[0] or len(ls) < 2:
+                    continue
+ 
+                new_image(*ls)
+ 
+ 
+if '__main__' == __name__:
+    new_image(400, 300, '方桦', show_image=True)
+    # new_image_with_file('image_data.txt')
