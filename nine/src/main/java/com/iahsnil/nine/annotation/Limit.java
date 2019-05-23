93011d3813695aa6b0b14075627cfa36200e79d9
DESKTOP-3M8EM7B\zed <iahsnil1994@gmail.com>
2019-05-23 17:48:09 +0700

    request limit

---
+++ nine/src/main/java/com/iahsnil/nine/annotation/Limit.java
@@ -0,0 +1,58 @@
+package com.iahsnil.nine.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @Author: zed
+ * @Date: 2019/5/23 10:33
+ * @Description: 限流注解
+ */
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+public @interface Limit {
+    /**
+     * 资源的名字
+     *
+     * @return String
+     */
+    String name() default "";
+
+    /**
+     * 资源的key
+     *
+     * @return String
+     */
+    String key() default "";
+
+    /**
+     * Key的prefix
+     *
+     * @return String
+     */
+    String prefix() default "";
+
+    /**
+     * 给定的时间段
+     * 单位秒
+     *
+     * @return int
+     */
+    int period();
+
+    /**
+     * 最多的访问限制次数
+     *
+     * @return int
+     */
+    int count();
+
+    /**
+     * 类型
+     *
+     * @return LimitType
+     */
+    LimitType limitType() default LimitType.CUSTOMER;
+}
+
--- nine/src/main/java/com/iahsnil/nine/config/CacheConfig.java
+++ nine/src/main/java/com/iahsnil/nine/config/CacheConfig.java
@@ -10,10 +10,13 @@ import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
 
 /**
  * @Author: zed
@@ -50,6 +53,15 @@ public class CacheConfig  extends CachingConfigurerSupport {
         jackson2JsonRedisSerializer.setObjectMapper(om);
         template.setValueSerializer(jackson2JsonRedisSerializer);
         template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, Serializable> limitRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Serializable> template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setConnectionFactory(redisConnectionFactory);
         return template;
     }
 }
