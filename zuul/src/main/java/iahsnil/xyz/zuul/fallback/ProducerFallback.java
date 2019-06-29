8d0e67d6ad1f606f32cd9dc3997e88af69e065c4
DESKTOP-3M8EM7B\zed <iahsnil1994@gmail.com>
2019-06-29 16:43:53 +0700

    zuul second commit

---
+++ zuul/src/main/java/iahsnil/xyz/zuul/fallback/ProducerFallback.java
@@ -0,0 +1,74 @@
+package iahsnil.xyz.zuul.fallback;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @Author: zed
+ * @Date: 2019/6/29 16:03
+ * @Description: 熔断返回内容
+ */
+@Component
+@Slf4j
+public class ProducerFallback implements FallbackProvider {
+
+    //指定要处理的service
+    @Override
+    public String getRoute() {
+        return "nine-producer";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        if (cause != null && cause.getCause() != null) {
+            String reason = cause.getCause().getMessage();
+            log.info("Excption {}",reason);
+        }
+        return fallbackResponse();
+    }
+
+    private ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("The service is unavailable.".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
